8c08a72f9b6b9c3e6590a1119359c7597948aaaa
Ted Tsai <ttbee.tw@gmail.com>
2023-05-23 06:42:26 +0800

    V1.0 Check Matrix Construction

--- Balanced-Motor-Matrix.py
+++ Balanced-Motor-Matrix.py
@@ -5,21 +5,19 @@ S = 12      # Slots number
 P = 10      # Pole number
 Nph = 3     # Phase number
 
-Ncph = int(S/Nph)       # Slots per phase
-Span = np.floor(S/P)    # Coil span
-E_angle = 180*P/S       # Electrical angle per slot
-Ncpp = S/P/Nph          # Slots per phase per pole
+Check = np.zeros((S,P), dtype=bool)
+for S in range(2,S):
+    for P in range(2,P): 
+        # Calculate K0
+        K0 = np.zeros(int(S/2))
+        for q in range(int(S/2)):
+            K0[q] = 2*S/Nph/P*(1+Nph*q)
+        print('S, P, K0 =', S, P, K0)
 
-print('Nominal Coil Span =', Span)
-
-# Calculate K0
-K0 = np.zeros(int(S/2))
-for q in range(int(S/2)):
-    # K0[q] = 2*S/3/P*(1+3*q)
-    K0[q] = 2*S/Nph/P*(1+Nph*q)
-print('Possible K0 =')
-print(K0)
-
-# Choose the smallest as K0
-K0 = np.min([int(x) for x in np.nditer(K0) if float(x).is_integer()])
-print('Choose K0 = ',K0)
+        # Check any int in K0
+        Any_int = any(float(x).is_integer() for x in np.nditer(K0))
+        # print('Any_int = ',Any_int)
+        
+        Check[S-1,P-1] = Any_int
+print('Check = ')
+print(Check)
